31e1f92f4928f7bf7d0caac2cf4d16d3c2475213
Cyril Zimodro <kzimodro@outlook.com>
2026-04-16 16:41:22 +0300

    Lab3: Added tests

--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -65,5 +65,65 @@ namespace NetSdrClientAppTests
         }
 
         //TODO: add more NetSdrMessageHelper tests
+        [Test]
+        public void TranslateMessage_ValidControlItem_ReturnsTrueAndParsesData()
+        {
+            // Arrange: Імітуємо правильне повідомлення SetControlItem (довжина 6 байт)
+            byte[] msg = { 0x06, 0x00, 0x20, 0x00, 0xAA, 0xBB }; 
+
+            // Act
+            bool success = NetSdrMessageHelper.TranslateMessage(msg, out var type, out var itemCode, out var sequenceNum, out var body);
+
+            // Assert
+            Assert.That(success, Is.True);
+            Assert.That(type, Is.EqualTo(NetSdrMessageHelper.MsgTypes.SetControlItem));
+            Assert.That(itemCode, Is.EqualTo(NetSdrMessageHelper.ControlItemCodes.ReceiverFrequency));
+            Assert.That(body, Is.EquivalentTo(new byte[] { 0xAA, 0xBB }));
+        }
+
+        [Test]
+        public void TranslateMessage_InvalidControlItemCode_ReturnsFalse()
+        {
+            // Arrange: Імітуємо повідомлення з неіснуючим кодом (0x9999)
+            byte[] msg = { 0x06, 0x00, 0x99, 0x99, 0xAA, 0xBB };
+
+            // Act
+            bool success = NetSdrMessageHelper.TranslateMessage(msg, out var type, out var itemCode, out _, out _);
+
+            // Assert
+            Assert.That(success, Is.False, "Метод має повернути false для невідомого коду");
+            Assert.That(itemCode, Is.EqualTo(NetSdrMessageHelper.ControlItemCodes.None));
+        }
+
+        [Test]
+        public void GetSamples_ValidInput_ReturnsCorrectIntegers()
+        {
+            // Arrange
+            ushort sampleSizeBits = 16; // 16 біт = 2 байти
+            byte[] body = { 0x01, 0x00, 0x02, 0x00, 0xFF, 0x00 }; // Три числа: 1, 2, 255
+
+            // Act
+            var samples = NetSdrMessageHelper.GetSamples(sampleSizeBits, body).ToList();
+
+            // Assert
+            Assert.That(samples.Count, Is.EqualTo(3));
+            Assert.That(samples[0], Is.EqualTo(1));
+            Assert.That(samples[1], Is.EqualTo(2));
+            Assert.That(samples[2], Is.EqualTo(255));
+        }
+
+        [Test]
+        public void GetSamples_TooLargeSampleSize_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            ushort invalidSampleSizeBits = 40; // 40 біт = 5 байт (а метод підтримує макс. 4)
+            byte[] body = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var samples = NetSdrMessageHelper.GetSamples(invalidSampleSizeBits, body).ToList();
+            });
+        }
     }
 }
