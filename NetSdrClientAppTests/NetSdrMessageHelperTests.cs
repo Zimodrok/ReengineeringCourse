067a8f411eba4f4ed65aa409c69ba7a5d957aa93
Cyril Zimodro <kzimodro@outlook.com>
2026-05-11 11:08:05 +0300

    Lab 3: Implement unit tests and coverage fix

--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -1,7 +1,11 @@
+using NUnit.Framework;
 using NetSdrClientApp.Messages;
+using System;
+using System.Linq;
 
 namespace NetSdrClientAppTests
 {
+    [TestFixture]
     public class NetSdrMessageHelperTests
     {
         [SetUp]
@@ -30,13 +34,14 @@ namespace NetSdrClientAppTests
             var actualCode = BitConverter.ToInt16(codeBytes.ToArray());
 
             //Assert
-            Assert.That(headerBytes.Count(), Is.EqualTo(2));
-            Assert.That(msg.Length, Is.EqualTo(actualLength));
-            Assert.That(type, Is.EqualTo(actualType));
-
-            Assert.That(actualCode, Is.EqualTo((short)code));
-
-            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+            Assert.Multiple(() =>
+            {
+                Assert.That(headerBytes.Count(), Is.EqualTo(2));
+                Assert.That(msg.Length, Is.EqualTo(actualLength));
+                Assert.That(type, Is.EqualTo(actualType));
+                Assert.That(actualCode, Is.EqualTo((short)code));
+                Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+            });
         }
 
         [Test]
@@ -57,13 +62,135 @@ namespace NetSdrClientAppTests
             var actualLength = num - ((int)actualType << 13);
 
             //Assert
-            Assert.That(headerBytes.Count(), Is.EqualTo(2));
-            Assert.That(msg.Length, Is.EqualTo(actualLength));
-            Assert.That(type, Is.EqualTo(actualType));
-
-            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+            Assert.Multiple(() =>
+            {
+                Assert.That(headerBytes.Count(), Is.EqualTo(2));
+                Assert.That(msg.Length, Is.EqualTo(actualLength));
+                Assert.That(type, Is.EqualTo(actualType));
+                Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+            });
         }
 
-        //TODO: add more NetSdrMessageHelper tests
+        [Test]
+        public void TranslateMessage_ValidControlItem_ReturnsTrueAndParsesData()
+        {
+            // Arrange
+            byte[] msg = { 0x06, 0x00, 0x20, 0x00, 0xAA, 0xBB }; 
+
+            // Act
+            bool success = NetSdrMessageHelper.TranslateMessage(msg, out var type, out var itemCode, out var sequenceNum, out var body);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(success, Is.True);
+                Assert.That(type, Is.EqualTo(NetSdrMessageHelper.MsgTypes.SetControlItem));
+                Assert.That(itemCode, Is.EqualTo(NetSdrMessageHelper.ControlItemCodes.ReceiverFrequency));
+                Assert.That(body, Is.EquivalentTo(new byte[] { 0xAA, 0xBB }));
+            });
+        }
+
+        [Test]
+        public void TranslateMessage_InvalidControlItemCode_ReturnsFalse()
+        {
+            // Arrange
+            byte[] msg = { 0x06, 0x00, 0x99, 0x99, 0xAA, 0xBB };
+
+            // Act
+            bool success = NetSdrMessageHelper.TranslateMessage(msg, out var type, out var itemCode, out _, out _);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(success, Is.False, "Метод має повернути false для невідомого коду");
+                Assert.That(itemCode, Is.EqualTo(NetSdrMessageHelper.ControlItemCodes.None));
+            });
+        }
+
+        [Test]
+        public void GetSamples_ValidInput_ReturnsCorrectIntegers()
+        {
+            // Arrange
+            ushort sampleSizeBits = 16; 
+            byte[] body = { 0x01, 0x00, 0x02, 0x00, 0xFF, 0x00 }; 
+
+            // Act
+            var samples = NetSdrMessageHelper.GetSamples(sampleSizeBits, body).ToList();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(samples.Count, Is.EqualTo(3));
+                Assert.That(samples[0], Is.EqualTo(1));
+                Assert.That(samples[1], Is.EqualTo(2));
+                Assert.That(samples[2], Is.EqualTo(255));
+            });
+        }
+
+        [Test]
+        public void GetSamples_TooLargeSampleSize_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            ushort invalidSampleSizeBits = 40; 
+            byte[] body = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var samples = NetSdrMessageHelper.GetSamples(invalidSampleSizeBits, body).ToList();
+            });
+        }
+        [Test]
+        public void GetHeader_DataItemMaxLength_ReturnsZeroLengthInHeader()
+        {
+            var type = NetSdrMessageHelper.MsgTypes.DataItem0;
+            byte[] parameters = new byte[8192]; // 8192 + 2 (header) = 8194
+
+            var result = NetSdrMessageHelper.GetDataItemMessage(type, parameters);
+
+            Assert.Multiple(() => {
+                Assert.That(result[0], Is.EqualTo(0x00));
+                Assert.That(result[1], Is.EqualTo(0x80));
+            });
+        }
+
+        [Test]
+        public void TranslateMessage_ShortMessage_ReturnsFalse()
+        {
+            byte[] shortMsg = { 0x01 }; 
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                    NetSdrMessageHelper.TranslateMessage(shortMsg, out _, out _, out _, out _);
+                });
+            }
+
+        [Test]
+        public void GetSamples_8BitSamples_HandlesPrefixBytes()
+        {
+            ushort sampleSizeBits = 8; 
+            byte[] body = { 0x01, 0x02, 0x03 }; 
+
+            var samples = NetSdrMessageHelper.GetSamples(sampleSizeBits, body).ToList();
+
+            Assert.Multiple(() => {
+                Assert.That(samples.Count, Is.EqualTo(3));
+                Assert.That(samples[0], Is.EqualTo(1));
+                Assert.That(samples[2], Is.EqualTo(3));
+            });
+        }
+
+        [Test]
+        public void GetSamples_24BitSamples_HandlesPrefixBytes()
+        {
+            ushort sampleSizeBits = 24; 
+            byte[] body = { 0x01, 0x00, 0x00, 0x02, 0x00, 0x00 }; 
+
+            var samples = NetSdrMessageHelper.GetSamples(sampleSizeBits, body).ToList();
+
+            Assert.Multiple(() => {
+                Assert.That(samples.Count, Is.EqualTo(2));
+                Assert.That(samples[0], Is.EqualTo(1));
+                Assert.That(samples[1], Is.EqualTo(2));
+            });
+        }
     }
 }
